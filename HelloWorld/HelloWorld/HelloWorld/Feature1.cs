15b085a3876a2cd057da7d695ba6fe798faaad84
KK Bhargava <kkbhargava1986@hotmail.com>
2020-09-20 18:43:29 +0530

    Refactored Add method.

--- HelloWorld/HelloWorld/HelloWorld/Feature1.cs
+++ HelloWorld/HelloWorld/HelloWorld/Feature1.cs
@@ -7,13 +7,9 @@ namespace HelloWorld
     class Feature1
     {
         // Code to call Feature 1
-        public int Add ()
+        public int Add (int x1, int x2)
         {
-            int x1 = 1;
-            int x2 = 2;
-
-            int sum = x1 + x2;
-            return sum;
+            return x1 + x2; ;
         }
     }
 }
