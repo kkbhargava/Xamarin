03d7051134d2fd628958781167a18ea6c579d266
KK Bhargava <kkbhargava1986@hotmail.com>
2020-09-20 18:38:30 +0530

    Added Add method.

--- HelloWorld/HelloWorld/HelloWorld/Feature1.cs
+++ HelloWorld/HelloWorld/HelloWorld/Feature1.cs
@@ -7,5 +7,13 @@ namespace HelloWorld
     class Feature1
     {
         // Code to call Feature 1
+        public int Add ()
+        {
+            int x1 = 1;
+            int x2 = 2;
+
+            int sum = x1 + x2;
+            return sum;
+        }
     }
 }
